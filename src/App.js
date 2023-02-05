c1dff6957d8cf5fc056818a7f31faffef103db5b
Asude Fışkın <74365269+asuf29@users.noreply.github.com>
2023-02-05 15:41:39 +0300

    toggleComplete

--- src/App.js
+++ src/App.js
@@ -4,14 +4,35 @@ import "./App.css";
 
 function App() {
   const [todos, setTodos] = useState([]);
+
+  const toggleComplete = (i) =>
+    setTodos(
+      todos.map((todo, k) =>
+        k === i
+          ? {
+              ...todo,
+              complete: !todo.complete,
+            }
+          : todo
+      )
+    );
+
   return (
     <div className="App">
       <Form
         onSubmit={(text) => setTodos([{ text, complete: false }, ...todos])}
       />
       <div>
-        {todos.map(({ text }) => (
-          <div key={text}>{text}</div>
+        {todos.map(({ text, complete }, i) => (
+          <div
+            key={text}
+            onClick={() => toggleComplete(i)}
+            style={{
+              textDecoration: complete ? "line-trought" : "",
+            }}
+          >
+            {text}
+          </div>
         ))}
       </div>
     </div>
